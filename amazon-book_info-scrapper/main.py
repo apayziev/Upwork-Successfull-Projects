871384fa5d52c41cf3ae1211b0408d444d5e94a3
Abdulkhay_Payziev <abdulkhay.payziev@gmail.com>
2023-01-05 10:30:21 +0500

    main function updated

--- amazon-book_info-scrapper/main.py
+++ amazon-book_info-scrapper/main.py
@@ -2,10 +2,11 @@ import time
 import random
 import requests
 from bs4 import BeautifulSoup
-from concurrent.futures import ProcessPoolExecutor
 from requests import Session
+from concurrent.futures import ProcessPoolExecutor
 from urllib3.util import Retry
 from tqdm import tqdm
+from requests.adapters import HTTPAdapter
 
 user_agents = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
@@ -33,62 +34,73 @@ def get_book_info(url):
 
     # Create a Session object and pass the Retry object to the Session object's mount method
     session = Session()
-    session.mount("http://", requests.adapters.HTTPAdapter(max_retries=retries))
+    session.mount("http://", HTTPAdapter(max_retries=retries))
 
     # Introduce a delay of 2 seconds between requests
     time.sleep(2)
 
-    try:
-        response = requests.get(url, headers=headers, timeout=10)
+    response = requests.get(url, headers=headers, timeout=10)
+
+    if response.status_code == 200:
         soup = BeautifulSoup(response.text, "lxml")
-    except Exception as e:
-        print(f"Error accessing URL {url}: {e}")
-        return None
 
-    title = soup.find("span", {"id": "productTitle"}).text.strip()
-    subtitle_element = soup.find("span", {"id": "productSubtitle"})
-    if subtitle_element:
-        subtitle = subtitle_element.text.strip()
-        full_title = f"{title} {subtitle}"
-    else:
-        full_title = title
+        # Extract book title from HTML
+        try:
+            title = soup.find("span", {"id": "productTitle"}).text.strip()
+        except AttributeError:
+            print(f" Error: Could not find book title for URL {url}")
+            return None
 
-    author_elements = soup.find_all("span", {"class": "author notFaded"})
-    authors = []
-    for author_element in author_elements:
-        link_element = author_element.find(
-            "a", {"class": "a-link-normal contributorNameID"}
-        )
-        if link_element:
-            authors.append(link_element.text.strip())
-        else:
-            link_element = author_element.find("a", {"class": "a-link-normal"})
-            authors.append(link_element.text.strip())
+        # Extract book authors from HTML
+        try:
+            author_elements = soup.find_all("span", {"class": "author notFaded"})
+            authors = []
+            for author_element in author_elements:
+                link_element = author_element.find(
+                    "a", {"class": "a-link-normal contributorNameID"}
+                )
+                if link_element:
+                    authors.append(link_element.text.strip())
+                else:
+                    link_element = author_element.find("a", {"class": "a-link-normal"})
+                    authors.append(link_element.text.strip())
+        except AttributeError:
+            print(f"Error: Could not find book authors for URL {url}")
+            return None
+        # Extract year of publication from HTML
+        try:
+            year_published = (
+                soup.find("div", {"id": "rpi-attribute-book_details-publication_date"})
+                .find(
+                    "div",
+                    {
+                        "class": "a-section a-spacing-none a-text-center rpi-attribute-value"
+                    },
+                )
+                .text
+            ).strip()
+        except AttributeError:
+            print(f"Error: Could not find year of publication for URL {url}")
+            return None
 
-    year_published = (
-        soup.find("div", {"id": "rpi-attribute-book_details-publication_date"})
-        .find(
-            "div",
-            {"class": "a-section a-spacing-none a-text-center rpi-attribute-value"},
-        )
-        .text
-    ).strip()
+        # Extract image URL from HTML
+        try:
+            image_element = soup.find("img", {"id": "ebooksImgBlkFront"})
+            if not image_element:
+                image_element = soup.find("img", {"id": "imgBlkFront"})
+            image_url = image_element["src"]
+        except AttributeError:
+            print(f"Error: Could not find image URL for URL {url}")
 
-    image_element = soup.find("img", {"id": "ebooksImgBlkFront"})
-    if not image_element:
-        image_element = soup.find("img", {"id": "imgBlkFront"})
-    if image_element:
-        image_url = image_element["src"]
-    else:
-        image_url = ""
+        return {
+            "title": title,
+            "authors": authors,
+            "year_published": year_published,
+            "image_url": image_url,
+        }
 
-    return {
-        "full_title": full_title,
-        "authors": authors,
-        "year_published": year_published,
-        "image_url": image_url,
-        "url": url,
-    }
+    print(f" Error: Could not get book info for URL {url}")
+    return None
 
 
 def main():
@@ -96,20 +108,19 @@ def main():
     with open("book_urls.txt", "r") as file:
         urls = file.readlines()
 
-    with open("book_info.txt", "w") as outfile:
-        with open("failed_urls.txt", "w") as failed_urls_file:
-            with ProcessPoolExecutor() as executor:
-                results = [executor.submit(get_book_info, url) for url in urls]
-                for result in tqdm(results):
-                    book_info = result.result()
-                    if book_info is None:
-                        failed_urls_file.write(f"{book_info['url']}\n")
-                    else:
-                        outfile.write(f"{book_info['full_title']}\n")
-                        outfile.write(", ".join(book_info["authors"]))
-                        outfile.write(f"\n{(book_info['year_published'])}\n")
-                        outfile.write(f"{book_info['image_url']}\n")
-                        outfile.write("\n")
+    with ProcessPoolExecutor() as executor:
+        book_infos = list(tqdm(executor.map(get_book_info, urls), total=len(urls)))
+
+    # Filter out None values
+    book_infos = list(filter(None, book_infos))
+
+    with open("book_infos.txt", "w") as file:
+        for book_info in book_infos:
+            file.write(f"{book_info['title']}\n")
+            file.write(", ".join(book_info["authors"]))
+            file.write(f"\n{book_info['year_published']}\n")
+            file.write(f"{book_info['image_url']}\n")
+            file.write("\n")
 
 
 if __name__ == "__main__":
