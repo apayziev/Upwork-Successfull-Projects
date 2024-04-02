274da4789ec0681bc799f1f8a01ac6ce0c5e3ff0
Abdulkhay Payziev <84310155+apayziev@users.noreply.github.com>
2024-04-02 16:44:29 +0500

    Update main.py

--- takelot-scraper/main.py
+++ takelot-scraper/main.py
@@ -3,6 +3,6 @@ from extract_product_links import extract_product_links
 
 if __name__ == "__main__":
     search_term = "Curcumin Supplements"
-    extract_product_links(search_term)
-    # scrape_product_info(search_term)
+    # extract_product_links(search_term)
+    scrape_product_info(search_term)
 
