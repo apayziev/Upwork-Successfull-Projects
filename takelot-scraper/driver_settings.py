79a94e98f4ff8b1c102d07e224c37d9b965b8c79
Abdulkhay Payziev <84310155+apayziev@users.noreply.github.com>
2024-04-03 16:55:56 +0500

    Update driver_settings.py

--- takelot-scraper/driver_settings.py
+++ takelot-scraper/driver_settings.py
@@ -1,7 +1,3 @@
-"""
-Module docstring goes here.
-"""
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -13,10 +9,19 @@ def initialize_driver():
     """Initializes the Chrome driver with the appropriate settings."""
     ua = UserAgent()
     chrome_options = Options()
-    # chrome_options.add_argument("--headless")  # Run Chrome in headless mode.
+    chrome_options.add_argument("--headless")  # Run Chrome in headless mode.
     chrome_options.add_argument(f"user-agent={ua.random}")  # Set the user agent.
     chrome_options.add_argument("--disable-notifications")  # Disable notifications.
     chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument("--disable-images")
+    chrome_options.add_argument("--disable-css")
+    # Re-enable JavaScript if necessary for dynamic content loading
+    chrome_options.add_argument("--enable-javascript")
+    chrome_options.add_argument("--disable-third-party-cookies")
+    chrome_options.add_argument("--enable-features=SameSiteByDefaultCookies,CookiesWithoutSameSiteMustBeSecure")
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    chrome_options.add_argument("--window-size=1280,800")
+
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
