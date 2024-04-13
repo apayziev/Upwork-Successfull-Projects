386f91e3092469f069ffc4bfe80b11f17f94fb2b
Abdulkhay Payziev <84310155+apayziev@users.noreply.github.com>
2024-04-13 08:33:45 +0500

    fix: failed urls

--- takelot-scraper/extract_product_info.py
+++ takelot-scraper/extract_product_info.py
@@ -35,7 +35,13 @@ def get_product_info(driver, product_link, search_term):
         driver.get(product_link)
 
         # Wait for main product section
-        wait_for_element(driver, (By.XPATH, '//*[contains(concat( " ", @class, " " ), concat( " ", "pdp-main-panel", " " ))]'), timeout=5)
+        wait_for_element(driver, (By.XPATH, '//*[contains(concat( " ", @class, " " ), '
+                                            'concat( " ", "pdp-main-panel", " " ))]'),
+                         timeout=5)
+
+        # response status check
+        response_status = driver.execute_script('return window.performance.getEntries()[0].response.status')
+        print(f"Response status: {response_status}")
 
         product_url = driver.current_url
 
@@ -46,7 +52,8 @@ def get_product_info(driver, product_link, search_term):
             pass  # Or retry if desired
 
         try:
-            login_button = driver.find_element(By.XPATH, '//*[contains(concat( " ", @class, " " ), concat( " ", "modal-module_close-button_asjao", " " ))]')
+            login_button = driver.find_element(By.XPATH, '//*[contains(concat( " ", @class, " " ), '
+                                                         'concat( " ", "modal-module_close-button_asjao", " " ))]')
             login_button.click()
         except NoSuchElementException:
             pass
@@ -58,13 +65,17 @@ def get_product_info(driver, product_link, search_term):
             logging.warning("Product name not found")
 
         try:
-            product_price = driver.find_element(By.XPATH, '//span[@class="currency plus currency-module_currency_29IIm" and @data-ref="buybox-price-main"]').text.strip()
+            product_price = driver.find_element(By.XPATH, '//span[@class="currency plus '
+                                                          'currency-module_currency_29IIm" and '
+                                                          '@data-ref="buybox-price-main"]').text.strip()
         except NoSuchElementException:
             product_price = None
 
         # Add to cart with enhanced wait
         try:
-            add_to_cart_button = wait_for_element(driver, (By.XPATH, "//a[contains(@class, 'add-to-cart-button-module_add-to-cart-button_1a9gT')]"), timeout=5)
+            add_to_cart_button = wait_for_element(driver, (By.XPATH, "//a[contains(@class, "
+                                                                      "'add-to-cart-button-module_add-to-cart-button_1a9gT')]"
+                                                                      ), timeout=5)
             add_to_cart_button.click()
         except (NoSuchElementException, TimeoutException):
             return {
@@ -77,7 +88,8 @@ def get_product_info(driver, product_link, search_term):
 
         # Wait for other elements involved in cart interactions
         time.sleep(1)  # Small delay after actions
-        go_to_cart_button = wait_for_element(driver, (By.XPATH, '//button[@class="button checkout-now dark"]'), timeout=5)
+        go_to_cart_button = wait_for_element(driver, (By.XPATH, '//button[@class="button checkout-now dark"]'),
+                                             timeout=5)
         go_to_cart_button.click()
 
         quantity_button = wait_for_element(driver, (By.XPATH, '//*[(@id = "cart-item_undefined")]'), timeout=5)
@@ -94,7 +106,8 @@ def get_product_info(driver, product_link, search_term):
         quantity_input.send_keys('9999')
 
         time.sleep(2)
-        update_button = wait_for_element(driver, (By.XPATH, '//*[contains(concat( " ", @class, " " ), concat( " ", "quantity-update", " " ))]'), timeout=5)
+        update_button = wait_for_element(driver, (By.XPATH, '//*[contains(concat( " ", @class, " " ), '
+                                                            'concat( " ", "quantity-update", " " ))]'), timeout=5)
         update_button.click()
 
         # Get message with error handling
@@ -106,7 +119,8 @@ def get_product_info(driver, product_link, search_term):
             logging.warning("Quantity notification not found")
 
         # remove item from cart
-        remove_button = driver.find_element(By.XPATH, '//*[contains(concat( " ", @class, " " ), concat( " ", "remove-item", " " ))]')
+        remove_button = driver.find_element(By.XPATH, '//*[contains(concat( " ", @class, " " ), '
+                                                      'concat( " ", "remove-item", " " ))]')
         remove_button.click()
 
         return {
@@ -126,12 +140,15 @@ def get_product_info(driver, product_link, search_term):
             "available_quantity": None
         }
 
-def process_product_link(product_link, search_term, queue):
-    """Processes a single product link and puts the result into a queue"""
+def process_product_link(product_link, search_term, queue, failure_queue):
+    """Processes a single product link and puts the result or failure into appropriate queues."""
+    driver = initialize_driver()
     try:
-        driver = initialize_driver()  # Initialize a new driver within the process
         product_info = get_product_info(driver, product_link, search_term)
         queue.put(product_info)
+    except (TimeoutException, NoSuchElementException) as e:
+        logging.error("Failed to process link %s: %s", product_link, str(e))
+        failure_queue.put(product_link)
     finally:
         driver.quit()
 
@@ -139,6 +156,9 @@ def scrape_product_info(search_term):
     """Scrapes product information for the given search term."""
     search_term_ = search_term.lower().replace(" ", "_")
     product_links_folder = "product_links"
+    outputs_folder = 'outputs'
+    current_date = pd.Timestamp.now().strftime('%Y-%m-%d')
+    file_name = f'{outputs_folder}/product_info_{search_term_}_{current_date}.csv'
 
     try:
         with open(f"{product_links_folder}/product_links_{search_term_}.txt", "r", encoding="utf-8") as f:
@@ -147,40 +167,44 @@ def scrape_product_info(search_term):
         logging.error("Product links file not found for search term '%s'", search_term)
         return
 
-    num_processes = max(1, mp.cpu_count() - 1)  # Reserve one CPU for the main process
+    num_processes = 5
+
+    def process_links(links):
+        with mp.Pool(processes=num_processes) as pool:
+            for product_link in links:
+                pool.apply_async(process_product_link, args=(product_link, search_term, queue, failure_queue))
+            pool.close()
+            pool.join()
 
     with mp.Manager() as manager:
-        queue = manager.Queue()  # Queue to store results from processes
+        queue = manager.Queue()
+        failure_queue = manager.Queue()
 
-        with mp.Pool(processes=num_processes) as pool:
-            for product_link in product_links:
-                pool.apply_async(process_product_link, args=(product_link, search_term, queue))
+        # Process all product links
+        process_links(product_links)
 
-            pool.close()
-            pool.join()  # Wait for all processes to finish
+        # Collect any failed links
+        failed_links = []
+        while not failure_queue.empty():
+            failed_links.append(failure_queue.get())
 
-            # Collect results from the queue
-            product_info = []
-            while not queue.empty():
-                product_info.append(queue.get())
+        # Retry failed links if there are any
+        if failed_links:
+            print(f"Retrying {len(failed_links)} failed links...")
+            process_links(failed_links)  # Re-use the process_links function for retries
 
-    # Create a DataFrame from the product information
+        # Collect all product info
+        product_info = []
+        while not queue.empty():
+            product_info.append(queue.get())
+
+    # Create DataFrame and save to CSV
     df = pd.DataFrame(product_info)
-
-    current_date = pd.Timestamp.now().strftime('%Y-%m-%d')
-    outputs_folder = 'outputs'
-    file_name = f'{outputs_folder}/product_info_{search_term_}_{current_date}.csv'
-
-    # Check if the outputs folder exists, create it if it doesn't
     if not os.path.exists(outputs_folder):
         os.makedirs(outputs_folder)
-
-    # Save the DataFrame to a CSV file in the outputs folder
     df.to_csv(file_name, index=False)
-
 
 if __name__ == "__main__":
     for search_term in search_terms:
         scrape_product_info(search_term)
         print(f"Finished scraping product information for '{search_term}'.")
-
